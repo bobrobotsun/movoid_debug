ff25a7863e60d817abdc94b8c7d8c06e920ee574
sunyifan0649 <JC0649@pmish-tech.com>
2024-06-14 18:09:30 +0800

    2024.06.14.01-JC0649 更新1.4.2.001 1、调整了部分逻辑，现在初始化时的显示更加合理 2、现在界面可以拖动，但是还没有真正地写好

--- movoid_debug/simple_ui/main_window.py
+++ movoid_debug/simple_ui/main_window.py
@@ -11,6 +11,8 @@ import traceback
 
 from PySide6.QtCore import Signal, Slot
 from PySide6.QtWidgets import QMainWindow, QApplication, QWidget, QGridLayout, QTreeWidget, QTextEdit, QHBoxLayout, QVBoxLayout, QPushButton, QTreeWidgetItem
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QMainWindow, QApplication, QTreeWidget, QTextEdit, QVBoxLayout, QPushButton, QTreeWidgetItem, QHeaderView, QSplitter, QWidget
 
 from .flow_thread import FlowThread
 from .value_set_window import ValueSetWindow, KeySetWindow, tree_item_can_expand, expand_tree_item_to_show_dir
@@ -56,85 +58,84 @@ class MainWindow(QMainWindow):
 
     def init_ui(self):
         screen_rect = QApplication.primaryScreen().geometry()
-        self.setGeometry(int(screen_rect.width() * 0.2), int(screen_rect.height() * 0.2), int(screen_rect.width() * 0.6), int(screen_rect.height() * 0.6))
-        main_table = QWidget(self)
-        self.setCentralWidget(main_table)
-        main_gird = QGridLayout(main_table)
-        main_table.setLayout(main_gird)
-        main_gird.setColumnStretch(0, 4)
-        main_gird.setColumnStretch(1, 2)
-        main_gird.setColumnStretch(2, 3)
-        main_gird.setColumnStretch(3, 1)
-        main_gird.setRowStretch(0, 1)
-        main_gird.setRowStretch(1, 2)
+        self.setGeometry(int(screen_rect.width() * 0.1), int(screen_rect.height() * 0.2), int(screen_rect.width() * 0.8), int(screen_rect.height() * 0.6))
+        main_splitter = QSplitter(self)
+        self.setCentralWidget(main_splitter)
+        main_splitter.setStretchFactor(0, 8)
+        main_splitter.setStretchFactor(1, 4)
+        main_splitter.setStretchFactor(2, 6)
+        main_splitter.setStretchFactor(3, 1)
 
-        flow_tree = QTreeWidget(main_table)
+        flow_tree = QTreeWidget(main_splitter)
         flow_tree.setObjectName('flow_tree')
-        main_gird.addWidget(flow_tree, 0, 0, 2, 1)
-        flow_tree.setHeaderLabels(['type', 'func', 'args', 'kwargs', 'status'])
-        flow_tree.itemClicked.connect(self.click_flow_refresh_ui)
+        main_splitter.addWidget(flow_tree)
+        flow_tree.setHeaderLabels(['type', 'func', 'status'])
+        flow_tree_header = flow_tree.header()
+        flow_tree_header.setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
+        flow_tree.itemClicked.connect(self.click_flow_tree_item)
 
-        print_text = QTextEdit(main_table)
+        error_text_splitter = QSplitter(Qt.Vertical, main_splitter)
+        main_splitter.addWidget(error_text_splitter)
+
+        print_text = QTextEdit(main_splitter)
         print_text.setObjectName('print_text')
-        main_gird.addWidget(print_text, 0, 1)
-
-        current_text = QTextEdit(main_table)
+        error_text_splitter.addWidget(print_text)
+        current_text = QTextEdit(main_splitter)
         current_text.setObjectName('current_text')
-        main_gird.addWidget(current_text, 1, 1, 2, 1)
+        error_text_splitter.addWidget(current_text)
 
-        arg_tree = QTreeWidget(main_table)
+        variable_tree_splitter = QSplitter(Qt.Vertical, main_splitter)
+        main_splitter.addWidget(variable_tree_splitter)
+
+        arg_tree = QTreeWidget(main_splitter)
         arg_tree.setObjectName('arg_tree')
-        main_gird.addWidget(arg_tree, 0, 2, 1, 1)
+        variable_tree_splitter.addWidget(arg_tree)
         arg_tree.setHeaderLabels(['arg', 'name', 'type', 'value'])
-        arg_tree.itemClicked.connect(lambda: self.click_arg_tree_item())
-        arg_tree.itemDoubleClicked.connect(lambda: self.change_arg_tree_value())
+        arg_tree.itemClicked.connect(self.click_arg_tree_item)
+        arg_tree.itemDoubleClicked.connect(self.change_arg_tree_value)
 
-        global_tree = QTreeWidget(main_table)
+        global_tree = QTreeWidget(main_splitter)
         global_tree.setObjectName('global_tree')
-        main_gird.addWidget(global_tree, 1, 2, 2, 1)
+        variable_tree_splitter.addWidget(global_tree)
         global_tree.setHeaderLabels(['key', 'type', 'value'])
         global_tree.itemExpanded.connect(self.expand_tree_item_to_show_dir)
 
-        run_widget = QWidget(main_table)
-        end_widget = QWidget(main_table)
-        main_gird.addWidget(run_widget, 0, 3, 3, 1)
-        main_gird.addWidget(end_widget, 4, 0, 1, 3)
+        run_widget = QWidget(main_splitter)
         run_grid = QVBoxLayout(run_widget)
         run_widget.setLayout(run_grid)
-        end_grid = QHBoxLayout(end_widget)
-        end_widget.setLayout(end_grid)
+        main_splitter.addWidget(run_widget)
 
-        run_test_button = QPushButton('测试', main_table)
+        run_test_button = QPushButton('测试', run_widget)
         run_test_button.setObjectName('run_test_button')
         run_grid.addWidget(run_test_button)
         run_test_button.clicked.connect(lambda: self.run_test())
         run_test_button.setEnabled(False)
         run_grid.addStretch(1)
 
-        add_args_button = QPushButton('新增args', main_table)
+        add_args_button = QPushButton('新增args', run_widget)
         add_args_button.setObjectName('add_args_button')
         run_grid.addWidget(add_args_button)
         add_args_button.clicked.connect(lambda: self.action_add_args())
         add_args_button.setEnabled(False)
-        delete_args_button = QPushButton('删除args', main_table)
+        delete_args_button = QPushButton('删除args', run_widget)
         delete_args_button.setObjectName('delete_args_button')
         run_grid.addWidget(delete_args_button)
         delete_args_button.clicked.connect(lambda: self.action_delete_args())
         delete_args_button.setEnabled(False)
         run_grid.addStretch(1)
 
-        change_kwargs_key_button = QPushButton('修改kwargs的key', main_table)
+        change_kwargs_key_button = QPushButton('修改kwargs的key', run_widget)
         change_kwargs_key_button.setObjectName('change_kwargs_key_button')
         run_grid.addWidget(change_kwargs_key_button)
         change_kwargs_key_button.clicked.connect(lambda: self.action_change_kwargs_key())
         change_kwargs_key_button.setEnabled(False)
 
-        add_kwargs_button = QPushButton('新增kwargs', main_table)
+        add_kwargs_button = QPushButton('新增kwargs', run_widget)
         add_kwargs_button.setObjectName('add_kwargs_button')
         run_grid.addWidget(add_kwargs_button)
         add_kwargs_button.clicked.connect(lambda: self.action_add_kwargs())
         add_kwargs_button.setEnabled(False)
-        delete_kwargs_button = QPushButton('删除kwargs', main_table)
+        delete_kwargs_button = QPushButton('删除kwargs', run_widget)
         delete_kwargs_button.setObjectName('delete_kwargs_button')
         run_grid.addWidget(delete_kwargs_button)
         delete_kwargs_button.clicked.connect(lambda: self.action_delete_kwargs())
@@ -142,18 +143,18 @@ class MainWindow(QMainWindow):
 
         run_grid.addStretch(3)
 
-        run_continue_button = QPushButton('忽略错误并continue', main_table)
+        run_continue_button = QPushButton('忽略错误并continue', run_widget)
         run_continue_button.setObjectName('run_continue_button')
         run_grid.addWidget(run_continue_button)
         run_continue_button.clicked.connect(lambda: self.run_continue())
         run_grid.addStretch(1)
 
-        run_raise_button = QPushButton('raise错误', main_table)
+        run_raise_button = QPushButton('raise错误', run_widget)
         run_raise_button.setObjectName('run_raise_button')
         run_grid.addWidget(run_raise_button)
         run_raise_button.clicked.connect(lambda: self.run_raise())
 
-        run_raise_one_button = QPushButton('raise错误至上一层', main_table)
+        run_raise_one_button = QPushButton('raise错误至上一层', run_widget)
         run_raise_one_button.setObjectName('run_raise_one_button')
         run_grid.addWidget(run_raise_one_button)
         run_raise_one_button.clicked.connect(lambda: self.run_raise_one())
@@ -161,8 +162,10 @@ class MainWindow(QMainWindow):
         run_grid.addStretch(6)
 
     def refresh_ui(self):
+        self.refresh_arg_tree(self.flow.current_function)
         self.refresh_flow_tree()
         self.refresh_global_tree()
+        self.refresh_current_text()
 
     def refresh_flow_tree(self):
         flow_tree: QTreeWidget = self.findChild(QTreeWidget, 'flow_tree')  # noqa
@@ -171,22 +174,19 @@ class MainWindow(QMainWindow):
         self.refresh_flow_tree_item(flow_tree, self.flow.main)
         current_function = self.flow.current_function
         print_text.setText(str(current_function.result(tostring=True)))
-        flow_tree.expandToDepth(1)
 
     def refresh_flow_tree_item(self, top_item, flow):
         arg_tree: QTreeWidget = self.findChild(QTreeWidget, 'arg_tree')  # noqa
-        select_flow = getattr(arg_tree, '__func', None)
+        select_func = getattr(arg_tree, '__func', None)
         for i in flow.son:
             if i[1] == 'function':
                 child = QTreeWidgetItem(top_item)
                 child.setText(0, i[0].func_type)
                 child.setText(1, i[0].func.__name__)
-                child.setText(2, str(i[0].args))
-                child.setText(3, str(i[0].kwargs))
-                child.setText(4, str(i[0].result(True, tostring=True)))
+                child.setText(2, str(i[0].result(True, tostring=True)))
                 setattr(child, '__func', i[0])
                 self.refresh_flow_tree_item(child, i[0])
-                if i[0] == select_flow:
+                if i[0] == select_func:
                     self.findChild(QTreeWidget, 'flow_tree').setCurrentItem(child)  # noqa
                     self.refresh_arg_tree(i[0])
             else:
@@ -194,77 +194,80 @@ class MainWindow(QMainWindow):
                 child.setText(0, 'log')
                 child.setText(1, str(i[0]))
 
-    def click_flow_refresh_ui(self, current_item):
+    def click_flow_tree_item(self, current_item):
+        self.refresh_arg_tree(getattr(current_item, '__func', None))
+        self.refresh_current_text()
+
+    def refresh_current_text(self):
         current_text: QTextEdit = self.findChild(QTextEdit, 'current_text')  # noqa
         arg_tree: QTreeWidget = self.findChild(QTreeWidget, 'arg_tree')  # noqa
-        arg_tree.clear()
-        current_func = getattr(current_item, '__func')
-        current_text.setText(str(current_func.result(tostring=True)))
-        self.refresh_arg_tree(current_func)
+        current_func = getattr(arg_tree, '__func', None)
+        if current_func is not None:
+            current_text.setText(str(current_func.result(tostring=True)))
 
     def refresh_arg_tree(self, func, kwarg_value=None):
-        kwarg_value = func.kwarg_value if kwarg_value is None else kwarg_value
-        arg_tree: QTreeWidget = self.findChild(QTreeWidget, 'arg_tree')  # noqa
-        run_test_button: QPushButton = self.findChild(QPushButton, 'run_test_button')  # noqa
-        add_args_button: QPushButton = self.findChild(QPushButton, 'add_args_button')  # noqa
-        add_kwargs_button: QPushButton = self.findChild(QPushButton, 'add_kwargs_button')  # noqa
-        run_test_button.setEnabled(not self.testing)
+        if func is not None:
+            kwarg_value = func.kwarg_value if kwarg_value is None else kwarg_value
+            arg_tree: QTreeWidget = self.findChild(QTreeWidget, 'arg_tree')  # noqa
+            run_test_button: QPushButton = self.findChild(QPushButton, 'run_test_button')  # noqa
+            add_args_button: QPushButton = self.findChild(QPushButton, 'add_args_button')  # noqa
+            add_kwargs_button: QPushButton = self.findChild(QPushButton, 'add_kwargs_button')  # noqa
+            run_test_button.setEnabled(not self.testing)
 
-        setattr(arg_tree, '__func', func)
-        setattr(arg_tree, '__kwarg_value', kwarg_value)
-        arg_tree.clear()
-        current_item = getattr(arg_tree, '__current_value', [None, None])
-        for k, v in kwarg_value['arg'].items():
-            temp = QTreeWidgetItem(arg_tree)
-            temp.setText(0, 'arg')
-            temp.setText(1, str(k))
-            temp.setText(2, type(v).__name__)
-            temp.setText(3, str(v))
-            setattr(temp, '__value', ['arg', k, v])
-            if current_item[0] == 'arg' and current_item[1] == k:
-                arg_tree.setCurrentItem(temp)
-        add_args_button.setEnabled('args' in kwarg_value)
-        if 'args' in kwarg_value:
-            args_name = list(kwarg_value['args'].keys())[0]
-            args_list = kwarg_value['args'][args_name]
-            for k, v in enumerate(args_list):
+            setattr(arg_tree, '__func', func)
+            setattr(arg_tree, '__kwarg_value', kwarg_value)
+            arg_tree.clear()
+            current_item = getattr(arg_tree, '__current_value', [None, None])
+            for k, v in kwarg_value['arg'].items():
                 temp = QTreeWidgetItem(arg_tree)
-                temp.setText(0, 'args')
-                temp.setText(1, f'{args_name}[{k}]')
+                temp.setText(0, 'arg')
+                temp.setText(1, str(k))
                 temp.setText(2, type(v).__name__)
                 temp.setText(3, str(v))
-                setattr(temp, '__value', ['args', args_name, k, v])
-                if current_item[0] == 'args' and current_item[1] == k:
+                setattr(temp, '__value', ['arg', k, v])
+                if current_item[0] == 'arg' and current_item[1] == k:
                     arg_tree.setCurrentItem(temp)
-        for k, v in kwarg_value['kwarg'].items():
-            temp = QTreeWidgetItem(arg_tree)
-            temp.setText(0, 'kwarg')
-            temp.setText(1, str(k))
-            temp.setText(2, type(v).__name__)
-            temp.setText(3, str(v))
-            setattr(temp, '__value', ['kwarg', k, v])
-            if current_item[0] == 'kwarg' and current_item[1] == k:
-                arg_tree.setCurrentItem(temp)
-        add_kwargs_button.setEnabled('kwargs' in kwarg_value)
-        if 'kwargs' in kwarg_value:
-            kwargs_name = list(kwarg_value['kwargs'].keys())[0]
-            kwargs_dict = kwarg_value['kwargs'][kwargs_name]
-            for k, v in kwargs_dict.items():
+            add_args_button.setEnabled('args' in kwarg_value)
+            if 'args' in kwarg_value:
+                args_name = list(kwarg_value['args'].keys())[0]
+                args_list = kwarg_value['args'][args_name]
+                for k, v in enumerate(args_list):
+                    temp = QTreeWidgetItem(arg_tree)
+                    temp.setText(0, 'args')
+                    temp.setText(1, f'{args_name}[{k}]')
+                    temp.setText(2, type(v).__name__)
+                    temp.setText(3, str(v))
+                    setattr(temp, '__value', ['args', args_name, k, v])
+                    if current_item[0] == 'args' and current_item[1] == k:
+                        arg_tree.setCurrentItem(temp)
+            for k, v in kwarg_value['kwarg'].items():
                 temp = QTreeWidgetItem(arg_tree)
-                temp.setText(0, 'kwargs')
-                temp.setText(1, f'{kwargs_name}[{k}]')
+                temp.setText(0, 'kwarg')
+                temp.setText(1, str(k))
                 temp.setText(2, type(v).__name__)
                 temp.setText(3, str(v))
-                setattr(temp, '__value', ['kwargs', kwargs_name, k, v])
-                if current_item[0] == 'kwargs' and current_item[1] == k:
+                setattr(temp, '__value', ['kwarg', k, v])
+                if current_item[0] == 'kwarg' and current_item[1] == k:
                     arg_tree.setCurrentItem(temp)
+            add_kwargs_button.setEnabled('kwargs' in kwarg_value)
+            if 'kwargs' in kwarg_value:
+                kwargs_name = list(kwarg_value['kwargs'].keys())[0]
+                kwargs_dict = kwarg_value['kwargs'][kwargs_name]
+                for k, v in kwargs_dict.items():
+                    temp = QTreeWidgetItem(arg_tree)
+                    temp.setText(0, 'kwargs')
+                    temp.setText(1, f'{kwargs_name}[{k}]')
+                    temp.setText(2, type(v).__name__)
+                    temp.setText(3, str(v))
+                    setattr(temp, '__value', ['kwargs', kwargs_name, k, v])
+                    if current_item[0] == 'kwargs' and current_item[1] == k:
+                        arg_tree.setCurrentItem(temp)
 
-    def click_arg_tree_item(self):
+    def click_arg_tree_item(self, current_item):
         arg_tree: QTreeWidget = self.findChild(QTreeWidget, 'arg_tree')  # noqa
         delete_args_button: QPushButton = self.findChild(QPushButton, 'delete_args_button')  # noqa
         delete_kwargs_button: QPushButton = self.findChild(QPushButton, 'delete_kwargs_button')  # noqa
         change_kwargs_key_button: QPushButton = self.findChild(QPushButton, 'change_kwargs_key_button')  # noqa
-        current_item = arg_tree.currentItem()
         current_value = getattr(current_item, '__value')
         delete_args_button.setEnabled(current_value[0] == 'args')
         delete_kwargs_button.setEnabled(current_value[0] == 'kwargs')
@@ -312,11 +315,10 @@ class MainWindow(QMainWindow):
         self.flow.raise_error = 1
         self.close()
 
-    def change_arg_tree_value(self):
+    def change_arg_tree_value(self, current_item):
         arg_tree: QTreeWidget = self.findChild(QTreeWidget, 'arg_tree')  # noqa
         func = getattr(arg_tree, '__func')
         kwarg_value = getattr(arg_tree, '__kwarg_value')
-        current_item = arg_tree.currentItem()
         current_value = getattr(current_item, '__value')
         new_value = ValueSetWindow.get_value(current_value[-1])
         if new_value != current_value[-1]:
