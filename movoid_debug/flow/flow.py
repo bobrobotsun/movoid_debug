c0575c2f76bd0311bfe401058f46fc081fdd3136
bobrobotsun <bobrobotsun@163.com>
2025-10-17 02:37:08 +0800

    2025.10.17 升级至1.4.11 新增了日常的触发 新增了no_debug功能 补充doc信息 新增force_raise功能，可以让raise error具有穿透性

--- movoid_debug/flow/flow.py
+++ movoid_debug/flow/flow.py
@@ -26,6 +26,15 @@ FLAG_RAISE = 1  # 当报错时，将error raise出去
 FLAG_PASS = 2  # 当报错时，将error储存起来，在适当时机再抛出
 
 
+def add_debug_doc(func, debug_default, debug_debug, force_raise):
+    debug_doc = f'''
+:param _debug_default:在不唤醒UI时，遇上error的处理逻辑，0/1为上报错误；2为跳过错误；默认{1 if debug_default is None else debug_default}, 
+:param _debug_debug：在会唤醒UI时，遇上error的处理逻辑，0为弹出UI进行处理；1为不弹出UI并向上报错；2为不弹出UI，也不向上报错；默认为{0 if debug_debug is None else debug_debug}
+:param _force_raise：设置为True后，可以让它的所有的子函数全部都主动raise error，而不是弹出窗口或跳过错误；默认为{False if force_raise is None else force_raise}
+    '''
+    func.__doc__ = func.__doc__ + debug_doc if func.__doc__ is not None else debug_doc
+
+
 def _time_point_to_str(time_float):
     re_str = ''
     if time_float:
@@ -60,6 +69,7 @@ class Flow:
         self.error_function = None
         self.test = False
         self._raise_error = 0
+        self.force_raise = 0
         self.raise_until_exit = False  # 这个参数不要开启，一旦修改为True，那么flow将尝试一直raise Error直到软件结束为止
         self.config = Config({
             'debug': {
@@ -118,10 +128,11 @@ class Flow:
         else:
             self.current_function = self.current_function.parent
 
-    def when_error(self, *debug_flag, err=None, trace_back=None):
+    def when_error(self, *debug_flag, force_raise=None, err=None, trace_back=None):
         """
         这是供FlowFunction反调的函数，保证当前的处理模式是预选模式
         :param debug_flag: 这是个传入的参数，就是把自己的参数传进去
+        :param force_raise: 把原函数的force raise传上来
         :param err: 把故障传上来
         :param trace_back: 把traceback信息传上来
         :return: 如果return 2 则是需要continue。如果return 1 则是需要 raise Error
@@ -129,7 +140,9 @@ class Flow:
         self.test = True
         self.error_function = self.current_function
         flag = self.analyse_target_debug_flag(*debug_flag)
-        if flag == FLAG_UI:  # 如果需要调用UI，那么要根据UI的结果决定最后的flag结论
+        if self.force_raise > 1 or self.force_raise == 1 and not force_raise:
+            re_value = FLAG_RAISE
+        elif flag == FLAG_UI:  # 如果需要调用UI，那么要根据UI的结果决定最后的flag结论
             self.get_error_step()
             re_value = self.when_error_window()
         else:  # 非UI情况下，不需要修改flag逻辑
@@ -324,7 +337,7 @@ class MainFunction(BasicFunction):
 class FlowFunction(BasicFunction):
     func_type = 'function'
 
-    def __init__(self, func, flow: Flow, include=None, exclude=None, teardown_function=None, debug_default: int = None, debug_debug: int = None):
+    def __init__(self, func, flow: Flow, include=None, exclude=None, teardown_function=None, debug_default: int = None, debug_debug: int = None, force_raise: bool = None):
         """
 
         """
@@ -347,17 +360,25 @@ class FlowFunction(BasicFunction):
         self.parent = flow.current_function
         self.debug_default = debug_default
         self.debug_debug = debug_debug
+        self.force_raise = force_raise if force_raise is not None else False
+        self.time_start = 0
+        self.time_self_end = 0
+        self.time_all_end = 0
 
     def __call__(self, *args, **kwargs):
-        debug_default = kwargs.pop('__debug_default', None)
-        debug_debug = kwargs.pop('__debug_debug', None)
+        debug_default = kwargs.pop('_debug_default', None)
+        debug_debug = kwargs.pop('_debug_debug', None)
+        force_raise = kwargs.pop('_force_raise', None)
         debug_default = self.debug_default if debug_default is None else debug_default
         debug_debug = self.debug_debug if debug_debug is None else debug_debug
+        force_raise = self.force_raise if force_raise is None else force_raise
         if self.flow.test:
-            test = TestFunction(func=self.func, flow=self.flow, ori=self, debug_default=debug_default, debug_debug=debug_debug)
-            return test(*args, **kwargs, __debug_default=debug_default, __debug_debug=debug_debug)
+            test = TestFunction(func=self.func, flow=self.flow, ori=self, debug_default=debug_default, debug_debug=debug_debug, force_raise=force_raise)
+            return test(*args, **kwargs, _debug_default=debug_default, _debug_debug=debug_debug, _force_raise=force_raise)
         else:
             try:
+                if force_raise:
+                    self.flow.force_raise += 1
                 self.args = args
                 self.kwargs = kwargs
                 self.kwarg_value = analyse_args_value_from_function(self.func, *args, **kwargs)
@@ -374,7 +395,7 @@ class FlowFunction(BasicFunction):
                     raise err
                 self.error = err
                 self.traceback = traceback.format_exc()
-                error_flag = self.flow.when_error(debug_default, debug_debug, err=self.error, trace_back=self.traceback)
+                error_flag = self.flow.when_error(debug_default, debug_debug, force_raise=force_raise, err=self.error, trace_back=self.traceback)
                 if error_flag == FLAG_RAISE:
                     self.flow.raise_error -= 1
                     raise err
@@ -386,6 +407,8 @@ class FlowFunction(BasicFunction):
                 self.has_return = True
                 self.re_value = re_value
             finally:
+                if force_raise:
+                    self.flow.force_raise -= 1
                 self.time_self_end = time.time()
                 teardown_args = dict(function=self.func, args=self.args, kwargs=self.kwargs, re_value=self.re_value, error=self.error, trace_back=self.traceback, has_return=self.has_return)
                 self.re_value = adapt_call(self.teardown_function, [], teardown_args, self.func, self.args, self.kwargs)
@@ -399,27 +422,32 @@ class FlowFunction(BasicFunction):
 class TestFunction(BasicFunction):
     func_type = 'test'
 
-    def __init__(self, func, flow, ori, debug_default=None, debug_debug=None):
+    def __init__(self, func, flow, ori, debug_default=None, debug_debug=None, force_raise=None):
         super().__init__()
         self.func = func
         self.flow = flow
         self.ori = ori
         self.debug_default = debug_default
         self.debug_debug = debug_debug
+        self.force_raise = force_raise if force_raise is not None else False
         self.parent = self.flow.current_function
         self.time_start = 0
         self.time_self_end = 0
         self.time_all_end = 0
 
     def __call__(self, *args, **kwargs):
-        debug_default = kwargs.pop('__debug_default', None)
-        debug_debug = kwargs.pop('__debug_debug', None)
+        debug_default = kwargs.pop('_debug_default', None)
+        debug_debug = kwargs.pop('_debug_debug', None)
+        force_raise = kwargs.pop('_force_raise', None)
         debug_default = self.debug_default if debug_default is None else debug_default
         debug_debug = self.debug_debug if debug_debug is None else debug_debug
+        force_raise = self.force_raise if force_raise is None else force_raise
         if self.end:
-            self.ori(*args, __debug_default=debug_default, __debug_debug=debug_debug, **kwargs)
+            return self.ori(*args, _debug_default=debug_default, _debug_debug=debug_debug, _force_raise=force_raise, **kwargs)
         else:
             try:
+                if force_raise:
+                    self.flow.force_raise += 1
                 self.args = args
                 self.kwargs = kwargs
                 self.kwarg_value = analyse_args_value_from_function(self.func, *args, **kwargs)
@@ -452,6 +480,8 @@ class TestFunction(BasicFunction):
                 if self.ori == self.flow.error_function:
                     self.ori.re_value = self.re_value
             finally:
+                if force_raise:
+                    self.flow.force_raise -= 1
                 self.time_self_end = time.time()
                 teardown_args = dict(function=self.func, args=self.args, kwargs=self.kwargs, re_value=self.re_value, error=self.error, trace_back=self.traceback, has_return=self.has_return)
                 self.re_value = adapt_call(self.ori.teardown_function, [], teardown_args, self.func, self.args, self.kwargs)
@@ -469,11 +499,35 @@ class TestError(Exception):
 FLOW = Flow()
 
 
-def debug(debug_default: int = None, debug_debug: int = None, include_error=None, exclude_error=None, teardown_function=None, force=False):
+def no_debug(force=True):
+    """
+    作为装饰器使用，使该函数不会被debug覆盖
+    :param force: 强制将debug 逻辑转换为当前逻辑
+    """
+    if callable(force):
+        return no_debug()(force)
+
+    def dec(func):
+        if force:
+            if getattr(func, '__debug', False):
+                func = getattr(func, '__function')
+        else:
+            if getattr(func, '__debug', False):
+                return func
+
+        setattr(func, '__debug', True)
+        setattr(func, '__function', func)
+        return func
+
+    return dec
+
+
+def debug(debug_default: int = None, debug_debug: int = None, force_raise: bool = None, include_error=None, exclude_error=None, teardown_function=None, force=False):
     """
     作为装饰器使用，使该函数会被debug覆盖
     :param debug_default: 默认情况下的处理方法，0→1
     :param debug_debug: debug状态下的处理方法，0
+    :param force_raise: 强制把它的子函数都转换为raise
     :param include_error: 仅抓取这些bug
     :param exclude_error: 不抓取这些bug
     :param teardown_function: 统一的teardown函数，需要传入参数、返回值、错误信息
@@ -483,20 +537,22 @@ def debug(debug_default: int = None, debug_debug: int = None, include_error=None
         return debug()(debug_default)
 
     def dec(func):
-        if getattr(func, '__debug', False):
-            if force:
+        if force:
+            if getattr(func, '__debug', False):
                 func = getattr(func, '__function')
-            else:
+        else:
+            if getattr(func, '__debug', False):
                 return func
 
         @wraps(func)
-        def wrapper(*args, __debug_default=debug_default, __debug_debug=debug_debug, **kwargs):
-            temp = FlowFunction(func, FLOW, include=include_error, exclude=exclude_error, teardown_function=teardown_function, debug_default=__debug_default, debug_debug=__debug_debug)
+        def wrapper(*args, _debug_default=debug_default, _debug_debug=debug_debug, _force_raise=force_raise, **kwargs):
+            temp = FlowFunction(func, FLOW, include=include_error, exclude=exclude_error, teardown_function=teardown_function, debug_default=_debug_default, debug_debug=_debug_debug, force_raise=_force_raise)
             re_value = temp(*args, **kwargs)
             return re_value
 
         setattr(wrapper, '__debug', True)
         setattr(wrapper, '__function', func)
+        add_debug_doc(wrapper, debug_default, debug_debug, force_raise)
         return wrapper
 
     return dec
