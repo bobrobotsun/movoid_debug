4ef4652bdb26ecbe713efe3de2d312f7f5c645b8
bobrobotsun <bobrobotsun@163.com>
2025-12-15 14:46:01 +0800

    2025.12.15 升级至1.5.4 暂时推一个可用版本 未来再添加一些用户友好的设置

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='movoid_debug',
-    version='1.5.3.3',
+    version='1.5.4',
     packages=find_packages(),
     url='',
     license='',
--- test/test_stack.py
+++ test/test_stack.py
@@ -1,13 +1,6 @@
 from movoid_function import STACK
-from movoid_debug import *
 
 
 class Test_class_Stack:
-    def test_01_STACK_initial_ignore_list(self):
-        assert len(STACK.ignore_list) == 12
+    def test_01_stack_self_check(self):
         STACK.self_check()
-
-    # def test_01_01_STACK_ui_ignore_list(self):
-    #     from movoid_debug import simple_ui
-    #     assert len(STACK.ignore_list) == 13
-    #     STACK.self_check()
